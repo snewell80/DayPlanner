58d13d70b3c1a5201e2981a564c0360ea7e1e487
snewell <snewell735@gmail.com>
2024-03-25 11:19:52 -0400

    Add project files.

---
+++ Data/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace DayPlanner.Data.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public String Owner { get; set; }
+        public string Title { get; set; } 
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CompleteDate { get; set; }
+
+        //will need crud operations
+    }
+}
